d1a81f9c2598bb41b306a6fb75fbc21820f7be41
Tim <pctimhk@gmail.com>
2020-05-07 18:37:41 +0800

    Update batch-convert.sh

--- batch-convert.sh
+++ batch-convert.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
-cd /convert/
-find ./ -name '*.heic' -or -name '*.HEIC'| while read i; do
+
+input=/photos/
+output=/output
+
+find $input -name '*.heic' -or -name '*.HEIC'| while read i; do
     [ -f "${i}" ] || break
-    if [ ! -f "${i%.*}".jpg ]; then
-        echo "$i -> ${i%.*}.jpg"
-        /tifig/build/tifig -v -p "$i" "${i%.*}".jpg
-        touch -r "$i" "${i%.*}".jpg
+    
+    filename="$(basename $i)"
+    filebasename="${filename%.*}"
+    folder="$(dirname $i)"
+    
+    outputfullpath="$output$folder$filebasename.jpg"
+    
+    if [ ! -f $outputfullpath ]; then
+        echo "$i -> $outputfullpath"
+        /tifig/build/tifig -v -p "$i" $outputfullpath
+        #touch -r "$i" $outputfullpath
     else
-        echo "${i%.*}.jpg already exists"
+        echo $outputfullpath already exists"
     fi
 done
