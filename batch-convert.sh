b58e7091f3aafb71364589b6b1b3f432e4792b01
Tim <pctimhk@gmail.com>
2020-05-08 11:28:05 +0800

    Update batch-convert.sh
     create parents directories if not exists.

--- batch-convert.sh
+++ batch-convert.sh
@@ -10,10 +10,15 @@ find $input -name '*.heic' -or -name '*.HEIC'| while read i; do
     filebasename="${filename%.*}"
     folder="$(dirname $i)"
     
-    outputfullpath="$output$folder$filebasename.jpg"
+    outputfolder="$output$folder"
+    outputfullpath="$outputfolder$filebasename.jpg"
     
     if [ ! -f $outputfullpath ]; then
-        echo "$i -> $outputfullpath"
+        if [ ! -d $outputfolder ]; then
+           mkdir -p $outputfolder
+           echo "$outputfolder created"
+        fi
+        echo "convert heic image from $i to $outputfullpath"
         /tifig/build/tifig -v -p "$i" $outputfullpath
         #touch -r "$i" $outputfullpath
     else
